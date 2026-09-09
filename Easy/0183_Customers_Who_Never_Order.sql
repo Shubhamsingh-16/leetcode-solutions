fb4c42a1b05e14bf973c6877e1ca51b86f49cc17
Shubham Singh <177919803+Shubhamsingh-16@users.noreply.github.com>
2026-09-09 22:18:09 +0530

    Sync LeetCode solution: Customers Who Never Order (MySQL)

--- Easy/0183_Customers_Who_Never_Order.sql
+++ Easy/0183_Customers_Who_Never_Order.sql
@@ -2,13 +2,11 @@
 -- Problem ID: 183
 -- Difficulty: Easy
 -- Language: MySQL
--- Runtime: 547 ms
+-- Runtime: 671 ms
 -- Memory: 0B
 -- Synced From: LeetCode
 -- Date: 2026-09-09
 
-# Write your MySQL query statement below
-Select c.name as Customers 
-From Customers c Left Join 
-Orders o on 
-c.id = o.customerId where o.customerId is null;
+select name as Customers
+from Customers
+where id not in (select customerId from Orders);
